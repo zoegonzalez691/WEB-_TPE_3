75d3f7c76333a482a3b811b5d9a8e3117c86e2fe
Tatiana Pereyra <tati25pereyra@gmail.com>
2024-11-12 12:07:21 -0300

    Se trabajo con metodo de autenticacion por tokens

--- app/controllers/user.controller.php
+++ app/controllers/user.controller.php
@@ -1,7 +1,29 @@
 <?php
+require_once 'app/models/user.model.php';
+require_once 'app/views/user.view.php';
+class UserApiController {
+    private $model;
+    private $view;
 
-class UserApiController{
+    public function __construct(){
+        $this->model = new UsersApiModel();
+        $this->view = new UserApiView();
+    }
 
+    public function verificarUser($req){
+        $UserName = $req->body->UserName;
+        $Password = $req->body->Password;
 
-    
+        $user = $this->model->GetUsuario($UserName);
+
+        if ($user && password_verify($Password, $user->contraseña)) {
+            // Generar token único
+            $token = bin2hex(random_bytes(16));
+            $this->model->SubirToken($user->usuario_id, $token);
+
+            return $this->view->response(['token' => $token], 200);
+        }
+
+        return $this->view->response("No se pudo autenticar el usuario", 404);
+    }
 }
--- app/models/user.model.php
+++ app/models/user.model.php
@@ -1,5 +1,42 @@
 <?php
+require_once 'config/config.php';
 
-class UserApiModel{
+class UsersApiModel{
+
+    private function CrearConexion(){
+        try{
+        $db =
+            new PDO(
+            "mysql:host=".dbHost.
+            ";dbname=".dbName.";charset=utf8", 
+            User, Password);
+        }catch(\Throwable $th) {
+            die($th);
+        }
+
+        return $db;
+    }
+
+    public function GetUsuario($nombre) {
+        $pdo = $this->CrearConexion();
+        $sql = "SELECT * FROM usuarios WHERE nombre = ?";
+        $query = $pdo->prepare($sql);
+        $query->execute([$nombre]);
     
+        $usuario = $query->fetch(PDO::FETCH_OBJ);
+    
+        return $usuario; 
+    
+    }
+
+    public function SubirToken($id, $token){
+        $pdo = $this->CrearConexion();
+        $sql = "UPDATE usuarios SET token = ? WHERE id = ?";
+        $query = $pdo->prepare($sql);
+        $query->execute([$token, $id]);
+
+    }
+
 }
+
+    
